e01a3a6e45163e7c3b79ec4cf29328f7871e0ca2
Ankit Sanghvi <ankit.sanghavi87@gmail.com>
2023-07-27 14:27:16 +0530

    setup project

---
+++ setup-script.sh
@@ -0,0 +1,4 @@
+conda install -c pytorch pytorch torchvision
+conda install cython scipy
+pip install -U 'git+https://github.com/cocodataset/cocoapi.git#subdirectory=PythonAPI'
+pip install git+https://github.com/cocodataset/panopticapi.git
